c45b2fbc9b18f3897f9f80b8f8dc42c745f6bf3e
Kiril Dobrev <KDobrev@indeavr.com>
2019-11-11 17:45:22 +0200

    🐛 FIX: linting

--- src/types/redux-store.ts
+++ src/types/redux-store.ts
@@ -1,11 +1,11 @@
 export type SomeStore = {
     someProp: string;
     anotherVal: string;
-}
+};
 
 export type AuthStore = {
     loginError?: string;
-}
+};
 
 export type ReduxStore = {
     some: SomeStore;
