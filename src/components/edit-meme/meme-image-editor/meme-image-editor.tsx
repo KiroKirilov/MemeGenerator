7d577c0d88d776e8e1c0e4d79efc0e1d1df111ad
Kiril Dobrev <KDobrev@indeavr.com>
2019-11-18 11:01:35 +0200

    👌 IMPROVE: Better image editor responsiveness

--- src/components/edit-meme/meme-image-editor/meme-image-editor.tsx
+++ src/components/edit-meme/meme-image-editor/meme-image-editor.tsx
@@ -7,7 +7,7 @@ import icona from "tui-image-editor/dist/svg/icon-a.svg";
 import iconb from "tui-image-editor/dist/svg/icon-b.svg";
 import iconc from "tui-image-editor/dist/svg/icon-c.svg";
 import icond from "tui-image-editor/dist/svg/icon-d.svg";
-import { memo } from "react";
+import { memo, useEffect } from "react";
 import "./meme-image-editor.scss";
 
 export const MemeImageEditor: React.FC = memo(() => {
@@ -19,6 +19,22 @@ export const MemeImageEditor: React.FC = memo(() => {
         "submenu.normalIcon.path": icond,
         "submenu.activeIcon.path": iconb,
     };
+
+    useEffect(() => {
+        const imageWidth: number = 750;
+        const imageHeight: number = 536;
+
+        const all: HTMLCollectionOf<Element> = document.getElementsByClassName("tui-image-editor-canvas-container");
+        for (let i: number = 0; i < all.length; i++) {
+            // @ts-ignore
+            all[i].style["margin-top"] = `${imageHeight / imageWidth * 30}%`;
+            // @ts-ignore
+            all[i].style["padding-top"] = `${imageHeight / imageWidth * 30}%`;
+            // @ts-ignore
+            all[i].style["padding-bottom"] = `${imageHeight / imageWidth * 70}%`;
+        }
+    }, []);
+
     return (
         <div>
             <ImageEditor
