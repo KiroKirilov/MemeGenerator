c837440c18ec7b07fc07d55fd191f79805ac3f12
Kiril Dobrev <KDobrev@indeavr.com>
2019-11-19 17:01:13 +0200

    👌 IMPROVE: Messing about with the canvas sizing

--- src/components/edit-meme/meme-image-editor/meme-image-editor.tsx
+++ src/components/edit-meme/meme-image-editor/meme-image-editor.tsx
@@ -22,46 +22,51 @@ export const MemeImageEditor: React.FC = memo(() => {
         "submenu.activeIcon.path": iconb,
     };
 
-    const imageUrl = "https://www.gannett-cdn.com/presto/2019/07/13/PROC/7ae05c5c-67da-4402-983f-6a3fccb42328-TY_071119_BLAZE_RED_PANDA_SENECA_PARK_ZOO.jpg";
-    // const imageUrl = "https://wallpaperaccess.com/full/797185.png";
+    // const imageUrl = "https://www.gannett-cdn.com/presto/2019/07/13/PROC/7ae05c5c-67da-4402-983f-6a3fccb42328-TY_071119_BLAZE_RED_PANDA_SENECA_PARK_ZOO.jpg";
+    const imageUrl = "https://cdn.blackmilkclothing.com/media/wysiwyg/Wallpapers/BM-Wallpapers-Oct_25-1.jpg";
 
     const editorRef = React.createRef<ImageEditorRef>();
 
     const loadImage = (imageSrc: string): Promise<HTMLImageElement> => {
         return new Promise((resolve, reject) => {
-            const image = new Image();
+            const image: HTMLImageElement = new Image();
 
             image.onload = () => {
                 resolve(image);
             };
 
             image.src = imageSrc;
-        })
-    }
-
+        });
+    };
     useEffect(() => {
-        async function setImageSize() {
+        const setImageSize: () => Promise<void> = async () => {
             if (editorRef.current) {
-                try {
-                    const image = await loadImage(editorRef.current.getInstance().toDataURL());
-    
-                    const imageWidth: number = image.width;
-                    const imageHeight: number = image.height;
-    
-                    const all: HTMLCollectionOf<Element> = document.getElementsByClassName("tui-image-editor-canvas-container");
-                    for (let i: number = 0; i < all.length; i++) {
-                        // @ts-ignore
-                        all[i].style["margin-top"] = `${imageHeight / imageWidth * 30}%`;
-                        // @ts-ignore
-                        all[i].style["padding-top"] = `${imageHeight / imageWidth * 30}%`;
-                        // @ts-ignore
-                        all[i].style["padding-bottom"] = `${imageHeight / imageWidth * 70}%`;
-                    }
-                } catch (error) {
-                    
-                }
+                // try {
+                //     const image: HTMLImageElement = await loadImage(imageUrl);
+
+                //     const imageWidth: number = image.height;
+                //     const imageHeight: number = image.width;
+                //     console.log(`width:${imageWidth}; height: ${imageHeight}`);
+
+                //     const canvasContainers: HTMLCollectionOf<Element> = document.getElementsByClassName("tui-image-editor-canvas-container");
+                //     for (let i: number = 0; i < canvasContainers.length; i++) {
+                //         // @ts-ignore
+                //         canvasContainers[i].style["margin-top"] = `${imageHeight / imageWidth * 70}%`;
+                //         // @ts-ignore
+                //         canvasContainers[i].style["padding-top"] = `${imageWidth / imageHeight * 35}%`;
+                //     }
+
+                //     const mainContainer: HTMLCollectionOf<Element> = document.getElementsByClassName("tui-image-editor-container bottom");
+                //     for (let i: number = 0; i < mainContainer.length; i++) {
+                //         const currentHeight: number = mainContainer[i].clientHeight;
+                //         // @ts-ignore
+                //         mainContainer[i].style.height = `${currentHeight + 145}px`;
+                //     }
+                // } catch (error) {
+                //     // ¯\_(ツ)_/¯ too bad
+                // }
             }
-        }
+        };
 
         setImageSize();
 
