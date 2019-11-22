4af63a0ab3559c3098417498c72f9c7c284e75fb
Kiril Dobrev <KDobrev@indeavr.com>
2019-11-22 09:09:59 +0200

    📦 NEW: Added error message to template picker

--- src/components/memes/meme-template-picker/meme-template-picker.module.scss.d.ts
+++ src/components/memes/meme-template-picker/meme-template-picker.module.scss.d.ts
@@ -1,4 +1,6 @@
 export const memeTemplateCardRow: string;
+export const memeTemplateCard: string;
 export default {} as {
   memeTemplateCardRow: string,
+  memeTemplateCard: string,
 }
--- src/components/memes/meme-template-picker/meme-template-picker.tsx
+++ src/components/memes/meme-template-picker/meme-template-picker.tsx
@@ -2,25 +2,27 @@ import * as React from "react";
 import { memo, useState } from "react";
 import { useSelector } from "react-redux";
 import { ReduxStore } from "../../../types/redux-store";
-import { Card } from "antd";
+import { Card, Alert } from "antd";
 import Meta from "antd/lib/card/Meta";
 import { MemeTemplate } from "../../../models/memes/meme-template";
 import { default as bootstrap } from "../../../common/styles/bootstrapGrid.module.scss";
 import { default as classes } from "./meme-template-picker.module.scss";
 import { StringHelpers } from "../../../helpers/string-helpers";
 import Search from "antd/lib/input/Search";
-import DebouncePromise from "awesome-debounce-promise";
+import DebouncePromise from "awesome-debounce-promise";;
 
-const delayBeforeSearch: number = 300;
+const delayBeforeUpdate: number = 300;
 
-function updateSearch(newValue: string, setSearchText: React.Dispatch<React.SetStateAction<string>>) {
+function updateSearch(newValue: string, setSearchText: React.Dispatch<React.SetStateAction<string>>): void {
     setSearchText(newValue);
 }
 
-const deboucedSearchUpdate = DebouncePromise(updateSearch, delayBeforeSearch);
+const deboucedSearchUpdate: (newValue: string, setSearchText: React.Dispatch<React.SetStateAction<string>>) => void
+    = DebouncePromise(updateSearch, delayBeforeUpdate);
 
 export const MemeTemplatePicker: React.FC = memo(() => {
     const templates: MemeTemplate[] = useSelector((store: ReduxStore) => store.memeTemplates.templates);
+    const hasErrored: boolean = useSelector((store: ReduxStore) => store.memeTemplates.hasErrored);
     const [searchText, setSearchText] = useState<string>("");
 
     function templateFilter(memeTemplate: MemeTemplate): boolean {
@@ -35,25 +37,42 @@ export const MemeTemplatePicker: React.FC = memo(() => {
 
     return (
         <div>
-            <div className={bootstrap.row}>
-                <div className={StringHelpers.joinClassNames(bootstrap.col12, classes.memeTemplateCardRow)}>
-                    <Search placeholder="Search for a template" onChange={e => deboucedSearchUpdate(e.target.value, setSearchText)} />
-                </div>
-            </div>
-            <div className={bootstrap.row}>
-                {
-                    filteredTemplates.map((t) => (
-                        <div key={t.url} className={StringHelpers.joinClassNames(bootstrap.col12, classes.memeTemplateCardRow)}>
-                            <Card
-                                hoverable
-                                cover={<img alt="example" src={t.url} />}
-                            >
-                                <Meta title={t.name} />
-                            </Card>
+            {
+                hasErrored
+                    ? (<div className={StringHelpers.joinClassNames(bootstrap.row, bootstrap.justifyContentCenter)}>
+                        <div className={bootstrap.col12}>
+                            <Alert
+                                message={"We couldn't load the popular templates :/"}
+                                type="error"
+                                showIcon
+                            />
                         </div>
-                    ))
-                }
-            </div>
+                    </div>)
+                    : (<>
+                        <div className={bootstrap.row}>
+                            <div className={StringHelpers.joinClassNames(bootstrap.col12, classes.memeTemplateCardRow)}>
+                                <Search
+                                    placeholder="Search for a template"
+                                    onChange={e => deboucedSearchUpdate(e.target.value, setSearchText)} />
+                            </div>
+                        </div>
+                        <div className={bootstrap.row}>
+                            {
+                                filteredTemplates.map((t) => (
+                                    <div key={t.url} className={StringHelpers.joinClassNames(bootstrap.col12, classes.memeTemplateCardRow)}>
+                                        <Card
+                                            className={classes.memeTemplateCard}
+                                            hoverable
+                                            cover={<img alt="example" src={t.url} />}
+                                        >
+                                            <Meta title={t.name} />
+                                        </Card>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </>)
+            }
         </div>
     );
 });
