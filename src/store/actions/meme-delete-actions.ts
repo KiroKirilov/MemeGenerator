57926b3d54ddb41cedb939674e26c4f9bd602924
Kiril Dobrev <KDobrev@indeavr.com>
2019-12-10 17:06:06 +0200

    📦 NEW: Meme deleted action implemented

--- src/store/actions/meme-delete-actions.ts
+++ src/store/actions/meme-delete-actions.ts
@@ -1,12 +1,14 @@
 import { FunctionAction } from "../../types/function-action";
 import { GetState } from "../../types/get-state";
 import { MemeDeleteActionType } from "../action-types/meme-delete/meme-delete-action-type";
+import { ExtendedFirestoreInstance } from "react-redux-firebase";
 
 export class MemeDeleteActions {
     public static memeDeleted(memeId: string): FunctionAction {
         return async (dispatch: any, getState: GetState, { getFirebase, getFirestore }) => {
             try {
-                console.log("deleted");
+                const firestore: ExtendedFirestoreInstance = getFirestore();
+                await firestore.doc(memeId).delete();
                 dispatch({ type: MemeDeleteActionType.MEME_DELETED, memeId: memeId });
             } catch (error) {
                 dispatch({ type: MemeDeleteActionType.MEME_DELETE_ERROR });
